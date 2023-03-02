0c509cddbacf1d1338ca0d0d2b3e991fce23daba
vaclav <st3jk46@gmail.com>
2023-03-02 19:17:16 +0100

    Ahoj

--- Documents/NetBeansProjects/PrvniAplikace/src/main/java/cz/itnetwork/prvniaplikace/PrvniAplikace.java
+++ Documents/NetBeansProjects/PrvniAplikace/src/main/java/cz/itnetwork/prvniaplikace/PrvniAplikace.java
@@ -11,6 +11,6 @@ package cz.itnetwork.prvniaplikace;
 public class PrvniAplikace {
 
     public static void main(String[] args) {
-        System.out.println("Hellsdfasdfabcdedsadsfadfsfdaso Wd!");
+        System.out.println("HellsdfasdfabcdeYYYYYdsadsfadfsfdaso Wd!");
     }
 }
