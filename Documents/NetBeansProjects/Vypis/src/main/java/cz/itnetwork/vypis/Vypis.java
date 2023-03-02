cb389787ba6803b5d0967ebbbd4a2b41d8e94f38
vaclav <st3jk46@gmail.com>
2023-03-02 19:31:41 +0100

    Ahoj

---
+++ Documents/NetBeansProjects/Vypis/src/main/java/cz/itnetwork/vypis/Vypis.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cz.itnetwork.vypis;
+
+/**
+ *
+ * @author Václav
+ */
+public class Vypis {
+
+    public static void main(String[] args) {
+        int a;
+        a = 56;
+        System.out.print(a);
+    }
+}
