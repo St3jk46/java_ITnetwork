025706714cf7e473d6d2e75234e7f4024385d2d9
vaclav <st3jk46@gmail.com>
2023-03-02 19:46:52 +0100

    Ahoj

--- Documents/NetBeansProjects/Papousek/src/main/java/cz/itnetwork/papousek/Papousek.java
+++ Documents/NetBeansProjects/Papousek/src/main/java/cz/itnetwork/papousek/Papousek.java
@@ -5,7 +5,13 @@ public class Papousek {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in, "Windows-1250");
-
+        System.out.println("Ahoj, jsem pták ohnivák, rád opakuji");
+        System.out.println("Napiš něco, člověku");
+        String vstup;
+        vstup = sc.nextLine();
+        String vystup;
+        vystup = vstup + ", " + vstup + "!";
+        System.out.println(vystup);
 
     }
 }
