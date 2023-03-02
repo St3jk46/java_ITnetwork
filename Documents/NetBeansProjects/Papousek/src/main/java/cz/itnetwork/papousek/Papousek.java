f254fe9a7ac34f70a7f9cc631e1f8af963c5fc9e
vaclav <st3jk46@gmail.com>
2023-03-02 19:41:28 +0100

    Ahoj

---
+++ Documents/NetBeansProjects/Papousek/src/main/java/cz/itnetwork/papousek/Papousek.java
@@ -0,0 +1,11 @@
+package cz.itnetwork.papousek;
+import java.util.Scanner;
+
+public class Papousek {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "Windows-1250");
+
+
+    }
+}
