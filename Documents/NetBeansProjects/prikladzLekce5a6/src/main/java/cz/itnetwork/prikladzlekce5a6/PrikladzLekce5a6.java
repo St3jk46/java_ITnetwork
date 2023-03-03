2ee0ba9155ed2c5f4093e5f4543b15ecb6cdb41a
vaclav <st3jk46@gmail.com>
2023-03-03 20:50:28 +0100

    )

--- Documents/NetBeansProjects/prikladzLekce5a6/src/main/java/cz/itnetwork/prikladzlekce5a6/PrikladzLekce5a6.java
+++ Documents/NetBeansProjects/prikladzLekce5a6/src/main/java/cz/itnetwork/prikladzlekce5a6/PrikladzLekce5a6.java
@@ -11,6 +11,11 @@ import java.util.Scanner;
 public class PrikladzLekce5a6 {
 
     public static void main(String[] args) {
+        
+/**
+ *
+ * Uppercasuje a pridava jedna k veku
+ */
         Scanner sc = new Scanner(System.in, "Windows-1250");
         System.out.println("Zadej jméno:");
         String jmeno = sc.nextLine();
@@ -22,6 +27,32 @@ public class PrikladzLekce5a6 {
         a = a + 1;
         System.out.println(jmeno.toUpperCase()+" "+prijmeni.toUpperCase());
         System.out.println("Za rok ti bude " + a + " let.");
+        
+/**
+ *
+ * Trimuje a porovnava
+ */       
+        
+        Scanner cs = new Scanner(System.in, "Windows-1250");
+        System.out.println("Zadejte delší slovo:");
+        String prvni = cs.nextLine().trim();
+        System.out.println("Zadejte kratší slovo:");
+        String druhe = cs.nextLine().trim();
+        int delkaprvni = prvni.length();
+        int delkadruhe = druhe.length();
+        int rozdil = delkaprvni - delkadruhe;
+        System.out.println("Slova se liší délkou o " + rozdil + " znaků");
+        
+/**
+ *
+ * Thleda string
+ */        
+        
+        Scanner css = new Scanner(System.in, "Windows-1250");
+        System.out.println("Zadej řetězec:");
+        String retezec = css.nextLine().trim().toLowerCase();
+        System.out.println(retezec);
+        System.out.println(retezec.contains("itnetwork"));
         
     }
 }
