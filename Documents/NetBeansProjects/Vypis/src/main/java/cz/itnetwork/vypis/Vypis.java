a35d50cff7c61d55318275ce91b4cb5b555fc8a1
vaclav <st3jk46@gmail.com>
2023-03-05 00:27:05 +0100

    Rovnovaha Vesmiru

--- Documents/NetBeansProjects/Vypis/src/main/java/cz/itnetwork/vypis/Vypis.java
+++ Documents/NetBeansProjects/Vypis/src/main/java/cz/itnetwork/vypis/Vypis.java
@@ -15,5 +15,13 @@ public class Vypis
 float a;
 a = 56.6F;
 System.out.println(a);
+
+System.out.println("Malá násobilka pomocí dvou cyklů:");
+for (double j = 0.01; j <= 1; j++) {
+    for (double i = 0.001; i <= 1; i++) {
+        System.out.print((i * j) + " ");
+    }
+    System.out.println();
+}
 	}
 }
