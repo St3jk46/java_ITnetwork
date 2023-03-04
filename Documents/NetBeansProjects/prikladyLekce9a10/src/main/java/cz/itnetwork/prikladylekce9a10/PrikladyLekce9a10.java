ef2e59d32557e354fab16a15e7fe9fc394821c74
vaclav <st3jk46@gmail.com>
2023-03-05 00:43:04 +0100

    Rovnovaha Vesmiru

---
+++ Documents/NetBeansProjects/prikladyLekce9a10/src/main/java/cz/itnetwork/prikladylekce9a10/PrikladyLekce9a10.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cz.itnetwork.prikladylekce9a10;
+import java.util.Scanner;
+/**
+ *
+ * @author Václav
+ */
+public class PrikladyLekce9a10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "Windows-1250");
+        System.out.println("Kolik ryb si dáš k večeři?");
+        String vstup = sc.nextLine();
+        int a = Integer.parseInt(vstup);
+        for (int i=0; i<a; i++){
+               System.out.println("<° )))-<");
+            }
+        }
+               
+    }
+
